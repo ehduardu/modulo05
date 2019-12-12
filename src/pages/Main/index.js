28491a84655278b0d899440718cad07c22045874
Dudu <dudsjrsilva@gmail.com>
2019-12-12 17:03:35 -0300

    Segundo Commit Aula Estilizando pagina Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return <Container></Container>;
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubimit = async e => {
+    e.preventDefault();
+
+    const { newRepo } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    console.log(response.data);
+  };
+
+  render() {
+    const { newRepo } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubtmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar Repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton disabled>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
