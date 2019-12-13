f817321e8b76c1969f2009ddf76f53e6bafbc19e
Dudu <dudsjrsilva@gmail.com>
2019-12-13 16:06:55 -0300

    Aula Exibindo issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-import { Loading } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 import Container from '../../components/Container';
 
 export default class Repository extends Component {
@@ -52,6 +53,30 @@ export default class Repository extends Component {
       return <Loading>Carregando</Loading>;
     }
 
-    return <Container>Repository</Container>;
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {/** LABELS */}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    );
   }
 }
